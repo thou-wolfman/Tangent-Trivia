3f819c394a194378c0f66e887a85a14279a7461e
thou-wolfman <47401503+thou-wolfman@users.noreply.github.com>
2019-02-11 18:51:21 -0600

    Add files via upload

---
+++ tangenttrivia.hyperesources/triviaInfotyler.js
@@ -0,0 +1,80 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"Tangent Trivia";
+	window.questions = [		"WHO IS THE DEVELOPER OF THE VIDEO GAME 'DESTINY'", 
+								"HOW MANY CHARACTER CLASSES ARE THERE IN THE VIDEO GAME'DESTINY'",
+								"WHO ARE THE MAIN CHARACTERS IN THE TV SHOW 'SUPERNATURAL'", 
+								"WHAT IS THE YEAR/MAKE OF THE BROTHERS' CAR IN THE TV SHOW 'SUPERNATURAL'", 
+								"WHAT IS A FATHERS WORST FEAR",
+								"WHO SINGS THE SONG 'BROWN EYED GIRL'",
+								"HOW MANY BONES ARE IN THE HUMAN HAND",
+								"IN 2017, WHICH COMPANY MADE THE MOST MONEY IN REVENUE",
+								"HOW MANY GIANT STORKS DID IT TAKE TO DELIVER PAUL BUNYAN TO HIS PARENTS",
+								"HOW MUCH MONEY DOES THE USA SPEND ON TOILET PAPER PER YEAR" ];
+	window.correctAnswer = [ 	"A", "C", "A", "A", "E", "D", "C", "E", "E", "D" ]; 
+	window.answerA = [ 			"BUNGIE",
+								"1",
+								"SAM AND DEAN",
+								"'67 IMPALA",
+								"DAUGHTERS BF",
+								"BEATLES",
+								"25",
+								"SONY",
+								"1",
+								"100K $" ]; 
+	window.answerB = [ 			"343 INDUSTRIES",
+								"2",
+								"CHUCK AND NORRIS",
+								"'68 MUSTANG",
+								"ANGRY WIFE",
+								"LED ZEPPELIN",
+								"26",
+								"APPLE",
+								"2",
+								"1M $" ];
+	window.answerC = [ 			"NAUGHTY DOG",
+								"3",
+								"RACHEL AND ROSS",
+								"'69 CYCLONE",
+								"ROGUE LEGO PIECES",
+								"RAY CHARLES",
+								"27",
+								"MICROSOFT",
+								"3",
+								"3B $" ];
+	window.answerD = [ 			"EA",
+								"4",
+								"DRAKE AND JOSH",
+								"'70 CHEVELLE",
+								"MORE KIDS",
+								"VAN MORRISON",
+								"28",
+								"ACTIVISION",
+								"4",
+								"6B $" ]; 
+	window.answerE = [ 			"BIOWARE",
+								"5",
+								"BEN AND CHRIS",
+								"'69 CHARGER",
+								"EVERYTHING",
+								"BEYONCE",
+								"29",
+								"TENCENT GAMES",
+								"5",
+								"10B $" ];
+										
+	window.response = [ 		"BUNGIE ALSO DEVELOPED HALO BACK IN THE DAY, BUT IS NOW RUN BY 343 INDUSTRIES", 
+								"THE 3 CLASSES ARE TITAN, WARLOCK, AND HUNTER. HUNTER IS THE BEST", 
+								"SAM AND DEAN WINCHESTER ARE NOT ONLY THE STRONGEST OF BROTHERS, BUT ALSO THE MOST DREAMY", 
+								"I WILL EVENTUALLY ACQUIRE A '67 IMPALA. BUT I MIGHT BE IN A VASE BY THEN", 
+								"FATHERS MUST FEAR EVERYTHING WHEN THEY HAVE KIDS",
+								"DO YOU REMEMBER WHEN, WE USE TO SING: SHA LA LA LA LA",
+								"WHEN THEY SAY A 'PIMP' HAND IS STRONG, THEY MEAN 27 BONES STRONG",
+								"OF COURSE THE ANSWER IS PRESSUMABLY THE LEAST KNOWN COMPANY ON THE LIST",
+								"I WONDER HOW MANY GIANT STORKS IT TOOK THEN TO GET BABE THE BLUE OX",
+								"MAN, THATS SOME DIRTY MONEY" ];
+							
+}
+						
